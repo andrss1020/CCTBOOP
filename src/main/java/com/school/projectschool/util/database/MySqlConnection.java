f5c8a3fd7c8e8cb85e2ad7e74a858e42b919781b
andres <andrs1020fima@hotmail.com>
2024-02-12 17:59:47 -0800

    Credentials database planetScale

--- src/main/java/com/school/projectschool/util/database/MySqlConnection.java
+++ src/main/java/com/school/projectschool/util/database/MySqlConnection.java
@@ -1,13 +1,29 @@
 package com.school.projectschool.util.database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MySqlConnection {
-    private static final String JDBC_URL = "jdbc:mysql://aws.connect.psdb.cloud/cctbmanagement?sslMode=VERIFY_IDENTITY";
-    private static final String USER = "cdv66852cgpdfnwt06yk";
-    private static final String PASSWORD = "pscale_pw_S4CZlp9imalLmNG0oxjh3yZBe2YCLVZqNF6yMt9FKeM";
+    private static final String JDBC_URL;
+    private static final String USER;
+    private static final String PASSWORD;
+
+    static {
+        Properties properties = new Properties();
+        try (InputStream input = MySqlConnection.class.getClassLoader().getResourceAsStream("database.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading database properties", e);
+        }
+
+        JDBC_URL = properties.getProperty("jdbc.url");
+        USER = properties.getProperty("db.user");
+        PASSWORD = properties.getProperty("db.password");
+    }
 
     public static Connection getConnection() {
         try {
@@ -15,7 +31,7 @@ public class MySqlConnection {
             return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-            throw new RuntimeException("Error connection database", e);
+            throw new RuntimeException("Error connecting to the database", e);
         }
     }
 }
